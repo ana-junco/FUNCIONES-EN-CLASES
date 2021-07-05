ae0eb440515589361b0e31d464592868ea5c5df7
Ana Junco <anajuncoescobar@gmail.com>
2021-07-05 15:51:46 -0500

    FUNCIONES EN CLASE

---
+++ cmake-build-debug/CMakeFiles/FUNCIONES_EN_CLASE.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FUNCIONES_EN_CLASE.dir/main.cpp.obj"
+  "FUNCIONES_EN_CLASE.exe"
+  "FUNCIONES_EN_CLASE.exe.manifest"
+  "FUNCIONES_EN_CLASE.pdb"
+  "libFUNCIONES_EN_CLASE.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FUNCIONES_EN_CLASE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
